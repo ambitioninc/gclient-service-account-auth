d668fd8ca7a12806f5dfce49dbde6109118a0799
Erik Swanson <theerikswanson@gmail.com>
2014-05-15 11:52:52 -0400

    setup.get_version fix.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_version():
     """
     Extracts the version number from the version.py file.
     """
-    VERSION_FILE = '{{ project_name }}/version.py'
+    VERSION_FILE = 'service_account_auth/version.py'
     mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
     if mo:
         return mo.group(1)
